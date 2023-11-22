66730016f33ef6be7559ae1fd483398513b8af82
madhu <madhu@Madhu143>
2023-11-22 22:09:54 +0530

    Second Commit

---
+++ Daytwo/src/com/tnsif/daytwo/BreakDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.daytwo;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+
+		for (int i = 5; i < 10; i++) {
+			if (i == 5)
+				
+			System.out.println(i);
+			break;
+		}
+
+	}
+
+}
